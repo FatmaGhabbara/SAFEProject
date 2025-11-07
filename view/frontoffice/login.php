c8363db2f7e0d04e981c3834c5697aa2e3ba9eb8
FatmaGhabbara <fatma.ezzahra@example.com>
2025-11-07 17:10:47 +0100

    Premier commit - Initialisation du projet SAFE

---
+++ view/frontoffice/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>login</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+    <body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header">
+
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="../backoffice/index.html" target="_blank">Admin</a></li>
+								<li><a href="elements.html">profile</a></li>
+								<li><a href="#">Sign Up</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+         <h1 class="text-center mb-4">Ajouter un Livre</h1>
+        <form method="POST" action="verification.php">
